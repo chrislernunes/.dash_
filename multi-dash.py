19468b77649eae0b665f81becac72dede36d4ed2
Chrisler Nunes <69667515+chrislernunes@users.noreply.github.com>
2023-07-23 22:06:22 +0530

    multi-dash.py

--- multi-dash.py
+++ multi-dash.py
@@ -1,34 +1,142 @@
-# Import necessary libraries
 import yfinance as yf
 import pandas as pd
+
 import dash
 from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
 
-# Function to get stock data and calculate moving averages
-# (Your existing code for these functions)
+# Set the option to display all rows
+pd.set_option('display.max_rows', None)
 
-# List of stock tickers (Feel free to add more or remove as needed)
-stock_tickers = ['ABCAPITAL.NS', 'ABB.NS', 'AARTIIND.NS', 'ASIANPAINT.NS', 'APOLLOTYRE.NS', 'ABFRL.NS', 'AUROPHARMA.NS', 'BANDHANBNK.NS', 'ABBOTINDIA.NS', 'AXISBANK.NS']
+# Fetch stock data
+stock_symbols = ['ABCAPITAL.NS', 'ABB.NS', 'AARTIIND.NS', 'ASIANPAINT.NS', 'APOLLOTYRE.NS', 'ABFRL.NS', 'AUROPHARMA.NS', 'BANDHANBNK.NS','ABBOTINDIA.NS', 'AXISBANK.NS', 'BATAINDIA.NS', 'BEL.NS']
 
-# Create the Dash application
+stock_data_df = pd.DataFrame()
+
+for symbol in stock_symbols:
+    stock_data = yf.download(symbol, start='2022-06-01')
+    stock_data["Symbol"] = symbol
+    stock_data_df = pd.concat([stock_data_df, stock_data], ignore_index=True)
+
+# Calculate the moving averages
+stock_data_df['10-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=10).mean().reset_index(0, drop=True).round(2)
+stock_data_df['20-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=20).mean().reset_index(0, drop=True).round(2)
+stock_data_df['50-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=50).mean().reset_index(0, drop=True).round(2)
+stock_data_df['150-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=150).mean().reset_index(0, drop=True).round(2)
+stock_data_df['200-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=200).mean().reset_index(0, drop=True).round(2)
+
+latest_data = stock_data_df.groupby('Symbol').tail(1).round(2)
+moving_avg_df = latest_data[['Symbol', 'Open', 'High', 'Low', 'Close', '10-day MA', '20-day MA', '50-day MA', '150-day MA', '200-day MA']]
+
+# Calculate the distance from each average to the current price as a percentage
+moving_avg_df['10-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['10-day MA']) / moving_avg_df['Close'] * 100).round(2)
+moving_avg_df['20-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['20-day MA']) / moving_avg_df['Close'] * 100).round(2)
+moving_avg_df['50-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['50-day MA']) / moving_avg_df['Close'] * 100).round(2)
+moving_avg_df['150-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['150-day MA']) / moving_avg_df['Close'] * 100).round(2)
+moving_avg_df['200-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['200-day MA']) / moving_avg_df['Close'] * 100).round(2)
+
+above_below_df = pd.DataFrame(index=['Above', 'Below'])
+
+# Calculate the total stocks above and below each moving average
+above_below_df['10-day MA'] = [len(moving_avg_df[moving_avg_df['10-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['10-day Distance'] >= 0])]
+above_below_df['20-day MA'] = [len(moving_avg_df[moving_avg_df['20-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['20-day Distance'] >= 0])]
+above_below_df['50-day MA'] = [len(moving_avg_df[moving_avg_df['50-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['50-day Distance'] >= 0])]
+above_below_df['150-day MA'] = [len(moving_avg_df[moving_avg_df['150-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['150-day Distance'] >= 0])]
+above_below_df['200-day MA'] = [len(moving_avg_df[moving_avg_df['200-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['200-day Distance'] >= 0])]
+
+# Create Dash application
 app = dash.Dash(__name__)
 server = app.server
 
-# Create the layout for the different pages using dcc.Tabs
+# Page 1 Layout
+page1_layout = html.Div([
+    html.H1('Moving Average Scanner'),
+    html.H2('Price Distance From Moving Averages'),
+    dash_table.DataTable(
+        id='moving-averages-table',
+        columns=[{"name": col, "id": col} for col in moving_avg_df.columns],
+        data=moving_avg_df.to_dict('records'),
+        style_cell={'textAlign': 'center'},
+        style_data_conditional=[
+            # Add your conditional styles here for moving averages distances
+        ],
+        sort_action='native',
+        sort_mode='single',
+        sort_by=[
+            {
+                'column_id': '10-day Distance',
+                'direction': 'asc',
+            }
+        ],
+        sort_as_null=True,
+    ),
+    html.H2('Stocks Above and Below Moving Averages'),
+    dash_table.DataTable(
+        id='above-below-table',
+        columns=[{"name": col, "id": col} for col in above_below_df.columns],
+        data=above_below_df.to_dict('records'),
+        style_cell={'textAlign': 'center'},
+    ),
+])
+
+# Page 2 Layout
+# List of stock tickers (Feel free to add more or remove as needed)
+stock_tickers = ['ABCAPITAL.NS', 'ABB.NS', 'AARTIIND.NS', 'ASIANPAINT.NS', 'APOLLOTYRE.NS', 'ABFRL.NS', 'AUROPHARMA.NS', 'BANDHANBNK.NS', 'ABBOTINDIA.NS', 'AXISBANK.NS']
+
+# Create an empty DataFrame to store the data for all stocks
+all_stocks_data = pd.DataFrame()
+
+# Fetch data for each stock and append it to the DataFrame
+for ticker in stock_tickers:
+    stock_data = get_stock_data(ticker)
+    all_stocks_data = all_stocks_data.append(stock_data.iloc[-1])
+
+# Reset the index of the DataFrame
+all_stocks_data.reset_index(inplace=True, drop=True)
+
+# Function to highlight crossing volume values in green font
+def highlight_crossing(s):
+    crossing_styles = [''] * len(s)
+    if s['Volume'] > s['3 Day Avg Volume']:
+        crossing_styles[5] = 'color: green;'
+    if s['Volume'] > s['Weekly Avg Volume']:
+        crossing_styles[6] = 'color: orange;'
+    if s['Volume'] > s['Monthly Avg Volume']:
+        crossing_styles[7] = 'color: blue;'
+    if s['Volume'] > s['Yearly Avg Volume']:
+        crossing_styles[8] = 'color: red;'
+    return crossing_styles
+
+# Apply the highlight function to the DataFrame
+highlighted_stocks_data = all_stocks_data.style.apply(highlight_crossing, axis=1)
+
+# Page 2 Layout
+page2_layout = html.Div([
+    html.H1('Stock Data with Volume Highlights'),
+    dash_table.DataTable(
+        id='stock-data-table',
+        columns=[{"name": col, "id": col} for col in highlighted_stocks_data.columns],
+        data=highlighted_stocks_data.to_dict('records'),
+        style_cell={'textAlign': 'center'},
+    ),
+])
+
+# Define callbacks for each page
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/page-1':
+        return page1_layout
+    elif pathname == '/page-2':
+        return page2_layout
+    else:
+        return page1_layout  # Set page1_layout as the default page
+
+# Create the main layout of the app
 app.layout = html.Div([
-    dcc.Tabs(id='tabs', value='moving-averages', children=[
-        dcc.Tab(label='Moving Averages', value='moving-averages'),
-        dcc.Tab(label='Stock Data', value='stock-data'),
-    ]),
+    dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
 
-# Callback to render the content for the selected tab
-@app.callback(Output('page-content', 'children'), [Input('tabs', 'value')])
-def render_content(tab):
-    # (Your existing code for rendering content based on selected tab)
-
 if __name__ == '__main__':
     app.run_server(debug=True)
-
