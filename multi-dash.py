77aeee83c28d256fece4d19a40a689414f8ba76e
Chrisler Nunes <69667515+chrislernunes@users.noreply.github.com>
2023-07-23 21:56:05 +0530

    multi-dash.py

--- multi-dash.py
+++ multi-dash.py
@@ -6,14 +6,7 @@ from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
 
 # Function to get stock data and calculate moving averages
-def get_stock_data(ticker):
-    # Your existing code for calculating moving averages here...
-    pass
-
-# Function to highlight crossing volume values in green font
-def highlight_crossing(s):
-    # Your existing code for highlighting crossing volume values here...
-    pass
+# (Your existing code for these functions)
 
 # List of stock tickers (Feel free to add more or remove as needed)
 stock_tickers = ['ABCAPITAL.NS', 'ABB.NS', 'AARTIIND.NS', 'ASIANPAINT.NS', 'APOLLOTYRE.NS', 'ABFRL.NS', 'AUROPHARMA.NS', 'BANDHANBNK.NS', 'ABBOTINDIA.NS', 'AXISBANK.NS']
@@ -34,37 +27,7 @@ app.layout = html.Div([
 # Callback to render the content for the selected tab
 @app.callback(Output('page-content', 'children'), [Input('tabs', 'value')])
 def render_content(tab):
-    if tab == 'moving-averages':
-        # Your existing code for moving averages table here...
-        pass
-    elif tab == 'stock-data':
-        # Create an empty DataFrame to store the data for all stocks
-        all_stocks_data = pd.DataFrame()
-
-        # Fetch data for each stock and append it to the DataFrame
-        for ticker in stock_tickers:
-            stock_data = get_stock_data(ticker)
-            all_stocks_data = all_stocks_data.append(stock_data.iloc[-1])
-
-        # Reset the index of the DataFrame
-        all_stocks_data.reset_index(inplace=True, drop=True)
-
-        # Apply the highlight function to the DataFrame
-        highlighted_stocks_data = all_stocks_data.style.apply(highlight_crossing, axis=1)
-
-        # Display the table with highlighted crossing volume values
-        return html.Div([
-            html.H1('Stock Data'),
-            dash_table.DataTable(
-                id='stock-data-table',
-                columns=[{"name": col, "id": col} for col in all_stocks_data.columns],
-                data=all_stocks_data.to_dict('records'),
-                style_cell={'textAlign': 'center'},
-                style_data_conditional=[
-                    # Apply any additional conditional styles as needed...
-                ],
-            ),
-        ])
+    # (Your existing code for rendering content based on selected tab)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
