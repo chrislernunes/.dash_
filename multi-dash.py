39d88c1a7d9b7513c56a4d144adbe635823bbdc6
Chrisler Nunes <69667515+chrislernunes@users.noreply.github.com>
2023-07-23 22:14:52 +0530

    multi-dash.py

--- multi-dash.py
+++ multi-dash.py
@@ -8,41 +8,50 @@ from dash.dependencies import Input, Output
 # Set the option to display all rows
 pd.set_option('display.max_rows', None)
 
-# Fetch stock data
-stock_symbols = ['ABCAPITAL.NS', 'ABB.NS', 'AARTIIND.NS', 'ASIANPAINT.NS', 'APOLLOTYRE.NS', 'ABFRL.NS', 'AUROPHARMA.NS', 'BANDHANBNK.NS','ABBOTINDIA.NS', 'AXISBANK.NS', 'BATAINDIA.NS', 'BEL.NS']
+# Function to fetch stock data
+def get_stock_data(ticker):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period="2y")
+    data['3 Day Avg Volume'] = data['Volume'].rolling(window=3).mean().fillna(0).round().astype(int)
+    data['Weekly Avg Volume'] = data['Volume'].rolling(window=5).mean().fillna(0).round().astype(int)
+    data['Monthly Avg Volume'] = data['Volume'].rolling(window=20).mean().fillna(0).round().astype(int)
+    data['Yearly Avg Volume'] = data['Volume'].rolling(window=252).mean().fillna(0).round().astype(int)
+    data['Ticker'] = ticker
 
-stock_data_df = pd.DataFrame()
+    # Calculate the change percentage and add it as a new column, rounded to 2 decimal points
+    data['Change %'] = (data['Close'].pct_change() * 100).fillna(0).round(2)
 
-for symbol in stock_symbols:
-    stock_data = yf.download(symbol, start='2022-06-01')
-    stock_data["Symbol"] = symbol
-    stock_data_df = pd.concat([stock_data_df, stock_data], ignore_index=True)
+    return data[['Ticker', 'Open', 'High', 'Low', 'Close', 'Change %', 'Volume', '3 Day Avg Volume', 'Weekly Avg Volume', 'Monthly Avg Volume', 'Yearly Avg Volume']]
 
-# Calculate the moving averages
-stock_data_df['10-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=10).mean().reset_index(0, drop=True).round(2)
-stock_data_df['20-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=20).mean().reset_index(0, drop=True).round(2)
-stock_data_df['50-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=50).mean().reset_index(0, drop=True).round(2)
-stock_data_df['150-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=150).mean().reset_index(0, drop=True).round(2)
-stock_data_df['200-day MA'] = stock_data_df.groupby('Symbol')['Close'].rolling(window=200).mean().reset_index(0, drop=True).round(2)
+# List of stock tickers (Feel free to add more or remove as needed)
+stock_tickers = ['ABCAPITAL.NS', 'ABB.NS', 'AARTIIND.NS', 'ASIANPAINT.NS', 'APOLLOTYRE.NS', 'ABFRL.NS', 'AUROPHARMA.NS', 'BANDHANBNK.NS', 'ABBOTINDIA.NS', 'AXISBANK.NS']
 
-latest_data = stock_data_df.groupby('Symbol').tail(1).round(2)
-moving_avg_df = latest_data[['Symbol', 'Open', 'High', 'Low', 'Close', '10-day MA', '20-day MA', '50-day MA', '150-day MA', '200-day MA']]
+# Create an empty DataFrame to store the data for all stocks
+all_stocks_data = pd.DataFrame()
 
-# Calculate the distance from each average to the current price as a percentage
-moving_avg_df['10-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['10-day MA']) / moving_avg_df['Close'] * 100).round(2)
-moving_avg_df['20-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['20-day MA']) / moving_avg_df['Close'] * 100).round(2)
-moving_avg_df['50-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['50-day MA']) / moving_avg_df['Close'] * 100).round(2)
-moving_avg_df['150-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['150-day MA']) / moving_avg_df['Close'] * 100).round(2)
-moving_avg_df['200-day Distance'] = ((moving_avg_df['Close'] - moving_avg_df['200-day MA']) / moving_avg_df['Close'] * 100).round(2)
+# Fetch data for each stock and append it to the DataFrame
+for ticker in stock_tickers:
+    stock_data = get_stock_data(ticker)
+    all_stocks_data = all_stocks_data.append(stock_data.iloc[-1])
 
-above_below_df = pd.DataFrame(index=['Above', 'Below'])
+# Reset the index of the DataFrame
+all_stocks_data.reset_index(inplace=True, drop=True)
 
-# Calculate the total stocks above and below each moving average
-above_below_df['10-day MA'] = [len(moving_avg_df[moving_avg_df['10-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['10-day Distance'] >= 0])]
-above_below_df['20-day MA'] = [len(moving_avg_df[moving_avg_df['20-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['20-day Distance'] >= 0])]
-above_below_df['50-day MA'] = [len(moving_avg_df[moving_avg_df['50-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['50-day Distance'] >= 0])]
-above_below_df['150-day MA'] = [len(moving_avg_df[moving_avg_df['150-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['150-day Distance'] >= 0])]
-above_below_df['200-day MA'] = [len(moving_avg_df[moving_avg_df['200-day Distance'] < 0]), len(moving_avg_df[moving_avg_df['200-day Distance'] >= 0])]
+# Function to highlight crossing volume values in green font
+def highlight_crossing(s):
+    crossing_styles = [''] * len(s)
+    if s['Volume'] > s['3 Day Avg Volume']:
+        crossing_styles[5] = 'color: green;'
+    if s['Volume'] > s['Weekly Avg Volume']:
+        crossing_styles[6] = 'color: orange;'
+    if s['Volume'] > s['Monthly Avg Volume']:
+        crossing_styles[7] = 'color: blue;'
+    if s['Volume'] > s['Yearly Avg Volume']:
+        crossing_styles[8] = 'color: red;'
+    return crossing_styles
+
+# Apply the highlight function to the DataFrame
+highlighted_stocks_data = all_stocks_data.style.apply(highlight_crossing, axis=1)
 
 # Create Dash application
 app = dash.Dash(__name__)
@@ -79,59 +88,13 @@ page1_layout = html.Div([
     ),
 ])
 
-# Define the get_stock_data function
-def get_stock_data(ticker):
-    stock = yf.Ticker(ticker)
-    data = stock.history(period="2y")
-    data['3 Day Avg Volume'] = data['Volume'].rolling(window=3).mean().fillna(0).round().astype(int)
-    data['Weekly Avg Volume'] = data['Volume'].rolling(window=5).mean().fillna(0).round().astype(int)
-    data['Monthly Avg Volume'] = data['Volume'].rolling(window=20).mean().fillna(0).round().astype(int)
-    data['Yearly Avg Volume'] = data['Volume'].rolling(window=252).mean().fillna(0).round().astype(int)
-    data['Ticker'] = ticker
-
-    # Calculate the change percentage and add it as a new column, rounded to 2 decimal points
-    data['Change %'] = (data['Close'].pct_change() * 100).fillna(0).round(2)
-
-    return data[['Ticker', 'Open', 'High', 'Low', 'Close', 'Change %', 'Volume', '3 Day Avg Volume', 'Weekly Avg Volume', 'Monthly Avg Volume', 'Yearly Avg Volume']]
-
-# Page 2 Layout
-# List of stock tickers (Feel free to add more or remove as needed)
-stock_tickers = ['ABCAPITAL.NS', 'ABB.NS', 'AARTIIND.NS', 'ASIANPAINT.NS', 'APOLLOTYRE.NS', 'ABFRL.NS', 'AUROPHARMA.NS', 'BANDHANBNK.NS', 'ABBOTINDIA.NS', 'AXISBANK.NS']
-
-# Create an empty DataFrame to store the data for all stocks
-all_stocks_data = pd.DataFrame()
-
-# Fetch data for each stock and append it to the DataFrame
-for ticker in stock_tickers:
-    stock_data = get_stock_data(ticker)
-    all_stocks_data = all_stocks_data.append(stock_data.iloc[-1])
-
-# Reset the index of the DataFrame
-all_stocks_data.reset_index(inplace=True, drop=True)
-
-# Function to highlight crossing volume values in green font
-def highlight_crossing(s):
-    crossing_styles = [''] * len(s)
-    if s['Volume'] > s['3 Day Avg Volume']:
-        crossing_styles[5] = 'color: green;'
-    if s['Volume'] > s['Weekly Avg Volume']:
-        crossing_styles[6] = 'color: orange;'
-    if s['Volume'] > s['Monthly Avg Volume']:
-        crossing_styles[7] = 'color: blue;'
-    if s['Volume'] > s['Yearly Avg Volume']:
-        crossing_styles[8] = 'color: red;'
-    return crossing_styles
-
-# Apply the highlight function to the DataFrame
-highlighted_stocks_data = all_stocks_data.style.apply(highlight_crossing, axis=1)
-
 # Page 2 Layout
 page2_layout = html.Div([
     html.H1('Stock Data with Volume Highlights'),
     dash_table.DataTable(
         id='stock-data-table',
-        columns=[{"name": col, "id": col} for col in highlighted_stocks_data.columns],
-        data=highlighted_stocks_data.to_dict('records'),
+        columns=[{"name": col, "id": col} for col in all_stocks_data.columns],
+        data=all_stocks_data.to_dict('records'),  # Use the DataFrame instead of the Styler object
         style_cell={'textAlign': 'center'},
     ),
 ])
@@ -155,3 +118,4 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
