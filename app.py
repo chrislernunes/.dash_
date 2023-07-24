0b7a52fc58302ae7f69a1f472070aa93c4f9506f
Chrisler Nunes <69667515+chrislernunes@users.noreply.github.com>
2023-07-24 20:52:55 +0530

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import html, dcc
 import yfinance as yf
 import pandas as pd
 
+
 # Set the option to display all rows
 pd.set_option('display.max_rows', None)
 
@@ -29,51 +30,56 @@ all_stocks_data = pd.DataFrame()
 # Fetch data for each stock and append it to the DataFrame
 for ticker in stock_tickers:
     stock_data = get_stock_data(ticker)
-    all_stocks_data = all_stocks_data._append(stock_data.iloc[-1])
-
-# Reset the index of the DataFrame
-all_stocks_data.reset_index(inplace=True, drop=True)
+    all_stocks_data = all_stocks_data.append(stock_data.iloc[-1], ignore_index=True)
 
 # Function to highlight crossing volume values in green font
-def highlight_crossing(s):
-    crossing_styles = [''] * len(s)
-    if s['Volume'] > s['3 Day Avg Volume']:
+def highlight_crossing(row):
+    crossing_styles = [''] * len(row)
+    if row['Volume'] > row['3 Day Avg Volume']:
         crossing_styles[6] = 'color: green;'
-    if s['Volume'] > s['Weekly Avg Volume']:
-        crossing_styles[7] = 'color: orange;'
-    if s['Volume'] > s['Monthly Avg Volume']:
-        crossing_styles[8] = 'color: blue;'
-    if s['Volume'] > s['Yearly Avg Volume']:
-        crossing_styles[9] = 'color: red;'
+    if row['Volume'] > row['Weekly Avg Volume']:
+        crossing_styles[7] = 'color: green;'
+    if row['Volume'] > row['Monthly Avg Volume']:
+        crossing_styles[8] = 'color: green;'
+    if row['Volume'] > row['Yearly Avg Volume']:
+        crossing_styles[9] = 'color: green;'
     return crossing_styles
 
 # Apply the highlight function to the DataFrame
-highlighted_stocks_data = all_stocks_data.style.apply(highlight_crossing, axis=1).set_table_styles([{
-    'selector': 'td',
-    'props': [
-        ('border-collapse', 'collapse'),
-        ('border', '1px solid gray'),
-        ('padding', '8px'),
-        ('text-align', 'left')
-    ]
-}])
+highlighted_stocks_data = all_stocks_data.apply(highlight_crossing, axis=1)
 
 # Create the Dash app
 app = dash.Dash(__name__)
 
 # Define the layout of the app
 app.layout = html.Div([
-    html_comp.Table(
-        # Convert the highlighted_stocks_data DataFrame to HTML
-        dangerously_allow_html=True,
-        children=html_comp.Tr(
-            [html_comp.Th(col) for col in highlighted_stocks_data.columns]
-        ) +
-        [
-            html_comp.Tr(
-                [html_comp.Td(val) for val in row[1:]]
-            ) for row in highlighted_stocks_data.iterrows()
-        ]
+    html.H1("Stock Data Table"),  # Add the title
+    dash_table.DataTable(
+        id='stock-table',
+        columns=[
+            {'name': col, 'id': col}
+            if col not in ('Ticker', 'Open', 'High', 'Low', 'Close')
+            else {'name': col, 'id': col, 'type': 'numeric', 'format': {'specifier': '.2f'}}
+            for col in all_stocks_data.columns
+        ],
+        data=all_stocks_data.to_dict('records'),
+        style_cell={
+            'textAlign': 'left',
+            'whiteSpace': 'normal',
+            'height': 'auto'
+        },
+        style_data_conditional=[
+            {'if': {'filter_query': '{Volume} > {`3 Day Avg Volume`}'}, 'color': 'green', 'fontWeight': 'bold'},
+            {'if': {'filter_query': '{Volume} > {`Weekly Avg Volume`}'}, 'color': 'orange', 'fontWeight': 'bold'},
+            {'if': {'filter_query': '{Volume} > {`Monthly Avg Volume`}'}, 'color': 'blue', 'fontWeight': 'bold'},
+            {'if': {'filter_query': '{Volume} > {`Yearly Avg Volume`}'}, 'color': 'red', 'fontWeight': 'bold'},
+        ],
+        style_header={
+            'fontWeight': 'bold'
+        },
+        filter_action='native',  # Enable native filtering
+        sort_action='native',    # Enable native sorting
+        sort_mode='multi'        # Allow multiple column sorting
     )
 ])
 
