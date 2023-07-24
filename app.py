e5fbe4a20ffdf4d8d5a7da7c73b53586094b0e7f
Chrisler Nunes <69667515+chrislernunes@users.noreply.github.com>
2023-07-24 19:30:14 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import dash
-from dash import dcc, html
-import pandas as pd
 import yfinance as yf
+import pandas as pd
 
-# Function to fetch stock data
+# Set the option to display all rows
+pd.set_option('display.max_rows', None)
+
 def get_stock_data(ticker):
     stock = yf.Ticker(ticker)
     data = stock.history(period="2y")
@@ -18,7 +18,7 @@ def get_stock_data(ticker):
 
     return data[['Ticker', 'Open', 'High', 'Low', 'Close', 'Change %', 'Volume', '3 Day Avg Volume', 'Weekly Avg Volume', 'Monthly Avg Volume', 'Yearly Avg Volume']]
 
-# Define the stock tickers
+# List of stock tickers (Feel free to add more or remove as needed)
 stock_tickers = ['ABCAPITAL.NS', 'ABB.NS', 'AARTIIND.NS', 'ASIANPAINT.NS', 'APOLLOTYRE.NS', 'ABFRL.NS', 'AUROPHARMA.NS', 'BANDHANBNK.NS', 'ABBOTINDIA.NS', 'AXISBANK.NS']
 
 # Create an empty DataFrame to store the data for all stocks
@@ -36,53 +36,17 @@ all_stocks_data.reset_index(inplace=True, drop=True)
 def highlight_crossing(s):
     crossing_styles = [''] * len(s)
     if s['Volume'] > s['3 Day Avg Volume']:
-        crossing_styles[7] = 'color: green;'
+        crossing_styles[5] = 'color: green;'
     if s['Volume'] > s['Weekly Avg Volume']:
-        crossing_styles[8] = 'color: orange;'
+        crossing_styles[6] = 'color: orange;'
     if s['Volume'] > s['Monthly Avg Volume']:
-        crossing_styles[9] = 'color: blue;'
+        crossing_styles[7] = 'color: blue;'
     if s['Volume'] > s['Yearly Avg Volume']:
-        crossing_styles[10] = 'color: red;'
+        crossing_styles[8] = 'color: red;'
     return crossing_styles
 
 # Apply the highlight function to the DataFrame
 highlighted_stocks_data = all_stocks_data.style.apply(highlight_crossing, axis=1)
 
-# Extract data from the Styler object and convert to DataFrame
-highlighted_stocks_data_df = highlighted_stocks_data.data
-
-# Create Dash application
-app = dash.Dash(__name__)
-server = app.server
-
-# Dash layout
-app.layout = html.Div([
-    html.H1("Stock Data Dashboard"),
-    dcc.Graph(
-        id='stock-table',
-        figure={
-            'data': [{
-                'type': 'table',
-                'header': {
-                    'values': highlighted_stocks_data_df.columns,
-                    'align': 'center',
-                    'fill': {'color': 'grey'},
-                    'line': {'color': 'white'}
-                },
-                'cells': {
-                    'values': highlighted_stocks_data_df.values.tolist(),
-                    'align': 'center',
-                    'fill': {'color': 'white'},
-                    'line': {'color': 'white'}
-                }
-            }],
-            'layout': {
-                'margin': {'l': 10, 'r': 10, 't': 50, 'b': 50}
-            }
-        }
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
+# Display the table with highlighted crossing volume values
+display(highlighted_stocks_data)
